18a7496059e67eedf08232589bd39df2da73b09a
Jos Ephraim <josephraimofficial@gmail.com>
2025-05-15 15:03:50 +0530

    created new user app(views and templates) set up urls.py in the root directory

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
-
+from users import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", views.register, name="register"),
     path("", include("blog.urls")),
-    path("blogs/",include("blogs.urls")),
-]   
+    path("blogs/", include("blogs.urls")),
+    path("blogs/", include("blogs.urls")),
+]
