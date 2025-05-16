36a2d6f4f8f64278cf160d44a7c746b824997721
Jos Ephraim <josephraimofficial@gmail.com>
2025-05-16 16:10:57 +0530

    styling for the nav bar to ensure correct buttons (logout and profile when they are logged out)

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .forms import userRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -19,5 +20,6 @@ def register(request):
     return render(request, "users/register.html", {"form": form})
 
 
+@login_required
 def profile(request):
     return render(request, "users/profile.html")
