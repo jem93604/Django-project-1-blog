70c370fcff0c8dbdbe0e6d71729c3770c388b07e
Jos Ephraim <josephraimofficial@gmail.com>
2025-05-29 22:35:58 +0530

    added a ai summarsing microservice using fastapi

---
+++ fastapi2.py
@@ -0,0 +1,28 @@
+from fastapi.middleware.cors import CORSMiddleware
+from llminference import (
+    llm_inference,
+)  # Assuming llminference is defined in this module
+
+# create the FastAPI app
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.post("/summarize")
+async def summarizewithllm(request: Request):
+    data = await request.json()
+    text = data.get("text", "")
+    text = llm_inference(text)
+    response = {
+        "summary": text,  # Placeholder summary logic
+    }
+    return response
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Django dev server
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
---
+++ llminference.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+def llm_inference(text):
+    response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": "Bearer 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",
+            "Content-Type": "application/json",
+            "HTTP-Referer": "<YOUR_SITE_URL>",  # Optional. Site URL for rankings on openrouter.ai.
+            "X-Title": "<YOUR_SITE_NAME>",  # Optional. Site title for rankings on openrouter.ai.
+        },
+        data=json.dumps(
+            {
+                "model": "mistralai/ministral-3b",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": f"Summarize the following text in a paragraph.{text}",
+                    },
+                ],
+            }
+        ),
+    )
+    # print the response text for debugging purposes
+    # print the response we got from the API
+    return response.json()["choices"][0]["message"]["content"]
